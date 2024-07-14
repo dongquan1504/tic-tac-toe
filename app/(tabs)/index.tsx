6cafb4bc36543620f28fde0cfffa03058afa5ac6
Dương Đông Quân <dongquan15041@gmail.com>
2024-07-14 20:44:06 +0700

    feat: build tic tac toe game

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,51 +1,73 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, TouchableOpacity, Button, View } from 'react-native';
+import React, { useState } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import GameScreen from '@/components/game';
 
 export default function HomeScreen() {
+  const [currentView, setCurrentView] = useState('home');
+
+  const renderContent = () => {
+    switch (currentView) {
+      case 'rules':
+        return <><ThemedText>
+          The game is played on a grid that's 3 squares by 3 squares.
+          You are X, your friend (or the computer in this case) is O.
+          Players take turns putting their marks in empty squares.
+          The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner.
+          When all 9 squares are full, the game is over.
+          If no player has 3 marks in a row, the game ends in a tie.
+        </ThemedText>
+          <ThemedText>
+            <ThemedText type="defaultSemiBold">Note</ThemedText>: When three pieces have been placed on the board, the fourth piece will replace the first one, meaning the first piece will disappear to make room for the fourth.
+          </ThemedText></>;
+      case 'playGame':
+        return <GameScreen />;
+      default:
+        // The default view is 'home'
+        return (
+          <>
+            <ThemedView style={styles.titleContainer}>
+              <ThemedText type="title">Welcome to tic-tac-toe!</ThemedText>
+              {/* <HelloWave /> */}
+            </ThemedView>
+            <Button
+              title="Play Game"
+              onPress={() => setCurrentView('playGame')}
+            />
+            <Button
+              title="Rules"
+              onPress={() => setCurrentView('rules')}
+            />
+          </>
+        );
+    }
+  };
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#737373' }}
       headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
+        <>
+          {currentView !== 'home' && (
+            <TouchableOpacity
+              style={styles.backIconContainer}
+              onPress={() => setCurrentView('home')}
+            >
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
+          )}
+          <View style={styles.imageCropContainer}>
+            <Image
+              source={require('@/assets/images/partial-react-logo.png')}
+              style={styles.reactLogoCropped}
+            />
+          </View>
+        </>
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      {renderContent()}
     </ParallaxScrollView>
   );
 }
@@ -60,11 +82,34 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
+  backIconContainer: {
+    position: 'absolute', // Adjust positioning as needed
+    top: 40, // Adjust according to your header's layout
+    left: 10, // Adjust according to your header's layout
+    zIndex: 10, // Ensure it's above other elements
+  },
+  backIcon: {
+    width: 24, // Adjust size as needed
+    height: 24, // Adjust size as needed
+  },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+  imageCropContainer: {
+    position: 'absolute',
+    width: 200, // Desired crop width
+    height: 200, // Desired crop height
+    overflow: 'hidden',
+    bottom: 0,
+  },
+  reactLogoCropped: {
+    width: "100%", // Actual image width after cropping
+    height: "100%", // Actual image height after cropping
+    position: 'relative',
+    bottom: -50,
   },
 });
---
+++ components/game.tsx
@@ -0,0 +1,174 @@
+import { Image, StyleSheet, TouchableOpacity, Button, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+interface SquareProps {
+  value: string | null;
+  isDisable: boolean;
+  onSquareClick: () => void;
+}
+interface BoardProps {
+  xIsNext: boolean;
+  squares: any[];
+  currentMove: number;
+  onPlay: (squares: any[]) => void;
+  onPlayAgain: () => void
+}
+
+function Square({ value, isDisable, onSquareClick }: SquareProps) {
+  return (
+    <TouchableOpacity style={styles.square} onPress={onSquareClick}>
+      <ThemedText type="title" darkColor={isDisable ? "#737373" : undefined}>
+        {value}
+      </ThemedText>
+    </TouchableOpacity>
+  );
+}
+
+function Board({ xIsNext, squares, currentMove, onPlay, onPlayAgain }: BoardProps) {
+  const [indexDeleteSquare, setIndexDeleteSquare] = useState<(number)[]>([]);
+
+  function handleClick(i: any) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    if (currentMove < 6) {
+      setIndexDeleteSquare([...indexDeleteSquare, i]);
+    } else {
+      nextSquares[indexDeleteSquare[0]] = null;
+      setIndexDeleteSquare([...indexDeleteSquare.slice(1), i]);
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedView>
+        <ThemedText>{status}</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.boardRow}>
+        <Square value={squares[0]} isDisable={(indexDeleteSquare[0] === 0) && currentMove >= 6} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} isDisable={(indexDeleteSquare[0] === 1) && currentMove >= 6} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} isDisable={(indexDeleteSquare[0] === 2) && currentMove >= 6} onSquareClick={() => handleClick(2)} />
+      </ThemedView>
+      <ThemedView style={styles.boardRow}>
+        <Square value={squares[3]} isDisable={(indexDeleteSquare[0] === 3) && currentMove >= 6} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} isDisable={(indexDeleteSquare[0] === 4) && currentMove >= 6} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} isDisable={(indexDeleteSquare[0] === 5) && currentMove >= 6} onSquareClick={() => handleClick(5)} />
+      </ThemedView>
+      <ThemedView style={styles.boardRow}>
+        <Square value={squares[6]} isDisable={(indexDeleteSquare[0] === 6) && currentMove >= 6} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} isDisable={(indexDeleteSquare[0] === 7) && currentMove >= 6} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} isDisable={(indexDeleteSquare[0] === 8) && currentMove >= 6} onSquareClick={() => handleClick(8)} />
+      </ThemedView>
+
+      {winner && <Button
+        title="Play Again"
+        onPress={onPlayAgain}
+      />}
+    </ThemedView>
+  );
+}
+
+function calculateWinner(squares: any[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default function GameScreen() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares: any) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function handlePlayAgain() {
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
+  }
+
+  // function jumpTo(nextMove) {
+  //   setCurrentMove(nextMove);
+  // }
+
+  // const moves = history.map((squares, move) => {
+  //   let description;
+  //   if (move > 0) {
+  //     description = 'Go to move #' + move;
+  //   } else {
+  //     description = 'Go to game start';
+  //   }
+  //   return (
+  //     <li key={move}>
+  //       <button onClick={() => jumpTo(move)}>{description}</button>
+  //     </li>
+  //   );
+  // });
+
+  return (
+    <ThemedView>
+      <ThemedView>
+        <Board xIsNext={xIsNext} squares={currentSquares} currentMove={currentMove} onPlay={handlePlay} onPlayAgain={handlePlayAgain} />
+      </ThemedView>
+      {/* <div className="game-info">
+        <ol>{moves}</ol>
+      </div> */}
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  boardRow: {
+    flexDirection: 'row',
+  },
+  square: {
+    width: 60, // Set the width of the square
+    height: 60, // Set the height of the square
+    backgroundColor: 'darkblue', // Set the background color of the square
+    justifyContent: 'center', // Center the content vertically
+    alignItems: 'center', // Center the content horizontally
+    margin: 10, // Add some margin around the square
+  },
+});
